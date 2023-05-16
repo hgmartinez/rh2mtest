9f37810ba8df9936788cdd3f28c89731ca0936eb
Henry Giovanni Martinez Cruz <hmartinezc@cadi.com.sv>
2023-05-16 09:17:14 -0600

    Priyecto Inicial

---
+++ src/main/java/org/com/pointsolutiondev/RHsystemApplication.java
@@ -0,0 +1,13 @@
+package org.com.pointsolutiondev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RHsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RHsystemApplication.class, args);
+	}
+
+}
